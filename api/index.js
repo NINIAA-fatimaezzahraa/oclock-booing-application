2de91f16d381e666fc50ef9ca981d69b39d198ec
NINIAA-fatimaezzahraa <niniaa.fatimaezzahraa@gmail.com>
2024-03-19 02:26:57 +0100

    Enhance RegisterPage and LoginPage with authentication logic : - implement user registration and login api using bcrypt for password hashing and JWT for session management. - introduce UserContext for global user state management across the React application. - configure Axios with baseURL and withCredentials for API communication. - update package.json with dependencies necessary for authentication (axios, bcryptjs, cookie-parser, dotenv, jsonwebtoken, mongoose).  - secure and streamline the authentication flow from the frontend to the backend.

--- api/index.js
+++ api/index.js
@@ -1,15 +1,84 @@
 const express = require('express');
 const cors = require('cors');
+const { default: mongoose } = require('mongoose');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
+
+const User = require('./models/User.js');
+
+const bcryptSalt = bcrypt.genSaltSync(10);
+const jwtSecret = 'fasefraw4r5r3wq45wdfgw34twdfg';
+
+require('dotenv').config();
 
 const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
 
 app.use(cors({
     credentials: true,
     origin: 'http://127.0.0.1:5173',
 }));
 
+mongoose.connect(process.env.MONGO_URL)
+
 app.get('/api/test', (req, res) => {
     res.json('test ok');
+});
+
+app.post('/api/register', async (req, res) => {
+    const { name, email, password } = req.body;
+    try {
+        const userDoc = await User.create({
+            name,
+            email,
+            password: bcrypt.hashSync(password, bcryptSalt)
+        });
+
+        console.log(name);
+        res.json(userDoc);
+    } catch (e) {
+        res.status(422).json(e);
+    }
+});
+
+app.post('/api/login', async (req, res) => {
+    const { email, password } = req.body;
+    const userDoc = await User.findOne({ email });
+
+    if (userDoc) {
+        const passOk = bcrypt.compareSync(password, userDoc.password);
+        if (passOk) {
+            jwt.sign({
+                email: userDoc.email,
+                id: userDoc._id
+            }, jwtSecret, {}, (err, token) => {
+                if (err) throw err;
+                res.cookie('token', token).json(userDoc);
+            });
+        } else {
+            res.status(422).json('password not ok');
+        }
+    } else {
+        res.json('user not found');
+    }
+});
+
+app.get('/api/profile', (req, res) => {
+    const { token } = req.cookies;
+    console.log(token);
+
+    if (token) {
+        jwt.verify(token, jwtSecret, {}, async (err, userData) => {
+            if (err) throw err;
+            const { name, email, _id } = await User.findById(userData.id);
+            res.json({ name, email, _id });
+        });
+    } else {
+        res.json(null);
+    }
 });
 
 app.listen(4000, () => {
