e54fbea9ddda115740bac0b162eb8e12b852f5d8
NINIAA-fatimaezzahraa <niniaa.fatimaezzahraa@gmail.com>
2024-03-22 03:33:34 +0100

    implement single place details page

---
+++ client/src/BookingWidget.jsx
@@ -0,0 +1,55 @@
+import {useContext, useEffect, useState} from "react";
+import {Navigate} from "react-router-dom";
+import {UserContext} from "./UserContext.jsx";
+
+export default function BookingWidget({place}) {
+  const [checkIn,setCheckIn] = useState('');
+  const [checkOut,setCheckOut] = useState('');
+  const [numberOfGuests,setNumberOfGuests] = useState(1);
+  const [name,setName] = useState('');
+  const [phone,setPhone] = useState('');
+  const [redirect,setRedirect] = useState('');
+  const {user} = useContext(UserContext);
+
+  useEffect(() => {
+    if (user) {
+      setName(user.name);
+    }
+  }, [user]);
+
+  if (redirect) {
+    return <Navigate to={redirect} />
+  }
+
+  return (
+    <div className="bg-white shadow p-4 rounded-2xl">
+      <div className="text-2xl text-center">
+        Price: ${place.price} / per night
+      </div>
+      <div className="border rounded-2xl mt-4">
+        <div className="flex">
+          <div className="py-3 px-4">
+            <label>Check in:</label>
+            <input type="date"
+                   value={checkIn}
+                   onChange={ev => setCheckIn(ev.target.value)}/>
+          </div>
+          <div className="py-3 px-4 border-l">
+            <label>Check out:</label>
+            <input type="date" value={checkOut}
+                   onChange={ev => setCheckOut(ev.target.value)}/>
+          </div>
+        </div>
+        <div className="py-3 px-4 border-t">
+          <label>Number of guests:</label>
+          <input type="number"
+                 value={numberOfGuests}
+                 onChange={ev => setNumberOfGuests(ev.target.value)}/>
+        </div>
+      </div>
+      <button className="primary mt-4">
+        Book this place
+      </button>
+    </div>
+  );
+}
